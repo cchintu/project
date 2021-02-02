e7ce187a0349e8f1fde47dc3eb724276a3c0ed18
cchintu <78419741+cchintu@users.noreply.github.com>
2021-02-02 18:19:16 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/excel",{useUnifiedTopology:true,useNewUrlParser:true
+mongoose.connect("mongodb+srv://cchinmaya3:Cchinmaya3@@cluster0.d6x1z.mongodb.net/Data?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true
 }).then(()=>{
     console.log('database connected');
 }).catch((e)=>{
